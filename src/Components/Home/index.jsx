66bedf12132d1d0003fb0f10df5eaf1b4a7c6a3f
santiagove7 <santiagovelo17@gmail.com>
2022-04-29 10:32:48 -0500

    Implementación arquitectura inicial

---
+++ src/Components/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles.module.scss'
+
+const Home = () => {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default Home
---
+++ src/Data/ProductsData.js
@@ -0,0 +1,38 @@
+export const ProductsData = [
+    {
+        id: 1,
+        price: 8.500,
+        amount: 0,
+        name: "Hamburguesa Sencilla",
+        img: "https://www.facebook.com/losperritoscr/photos/5170851442972108",
+    },
+    {
+        id: 2,
+        price: 14.500,
+        amount: 0,
+        name: "Hamburguesa Super",
+        img: "https://www.facebook.com/losperritoscr/photos/5186748628049056",
+    },
+    {
+        id: 3,
+        price: 8.500,
+        amount: 0,
+        name: "Perro Sencillo",
+        img: "https://www.facebook.com/losperritoscr/photos/3114213075302632",
+    },
+    {
+        id: 4,
+        price: 12.500,
+        amount: 0,
+        name: "Perro Super",
+        img: "hhttps://www.facebook.com/losperritoscr/photos/3108226559234617",
+    },
+    {
+        id: 5,
+        price: 5.900,
+        amount: 0,
+        name: "Porción de Papas",
+        img: "https://www.facebook.com/losperritoscr/photos/5070076126382974",
+    },
+
+]
