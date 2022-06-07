ed970ba08f29bf2566383bd9b21a3ed527dc16c2
santiagove7 <santiagovelo17@gmail.com>
2022-06-07 16:14:24 -0500

    Corrección Imagenes

--- src/Data/ProductsData.js
+++ src/Data/ProductsData.js
@@ -4,14 +4,14 @@ export const ProductsData = [
         price: 8.500,
         amount: 0,
         name: "Hamburguesa Sencilla",
-        img: "https://scontent.feoh6-1.fna.fbcdn.net/v/t39.30808-6/277810493_5170851426305443_6728570806457372075_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeGCmhGbHk_9ykJ09fPIqLAwwIvP0tdFJZjAi8_S10UlmIme3nNIcSp5FNtJCvgD56Cx-OogLDt1YptH5PP3twO8&_nc_ohc=ZU1avRtMo-IAX_kzMqM&_nc_ht=scontent.feoh6-1.fna&oh=00_AT_7b8jI7YiakgmyHsNDUNsSzPjdpypMEbeWjzWiB6pfsA&oe=629E9D01",
+        img: "https://scontent.feoh2-1.fna.fbcdn.net/v/t39.30808-6/277810493_5170851426305443_6728570806457372075_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeGCmhGbHk_9ykJ09fPIqLAwwIvP0tdFJZjAi8_S10UlmIme3nNIcSp5FNtJCvgD56Cx-OogLDt1YptH5PP3twO8&_nc_ohc=PocUr0MH38kAX_GCb1Y&_nc_ht=scontent.feoh2-1.fna&oh=00_AT9IappdfOOMOZ6yXeUWnuR0Z15JcNBLGl3FbGuExyHYOA&oe=62A48BC1",
     },
     {
         id: 2,
         price: 14.500,
         amount: 0,
         name: "Hamburguesa Super",
-        img: "https://scontent.feoh6-1.fna.fbcdn.net/v/t39.30808-6/278206568_5186748618049057_3561298297872125869_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeH1CQjHv3w615W4E86J_NrsudFYUByRw9G50VhQHJHD0UI7_Az-TQfsjq9_9qdFGB5b53woykEBL4lbVEE_c3UH&_nc_ohc=LGx2C54fF9kAX8Y7r-r&_nc_ht=scontent.feoh6-1.fna&oh=00_AT-74eLKU8QxUbIRs0tmAedjabsAZa0hFcz9RyUxbFDi2w&oe=629F4CB3",
+        img: "https://scontent.feoh2-1.fna.fbcdn.net/v/t39.30808-6/278206568_5186748618049057_3561298297872125869_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeH1CQjHv3w615W4E86J_NrsudFYUByRw9G50VhQHJHD0UI7_Az-TQfsjq9_9qdFGB5b53woykEBL4lbVEE_c3UH&_nc_ohc=K7-VjtPiJLYAX8CaXDU&_nc_ht=scontent.feoh2-1.fna&oh=00_AT_NA1qyD9KYiWx1eCBQtOJI2NGMQZpGG1sBZfWlylHVfg&oe=62A53B73",
     },
     {
         id: 3,
@@ -25,14 +25,14 @@ export const ProductsData = [
         price: 12.500,
         amount: 0,
         name: "Perro Super",
-        img: "https://scontent.feoh6-1.fna.fbcdn.net/v/t39.30808-6/274554932_5063805380343382_5018588772167601835_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeEtF-ymBimvMLdHgF5FrVq1sKgb8UIOExqwqBvxQg4TGjxFBJTxbwp_kb3pbcZ_C8dbeGXecKuyzgu-0O7w1q3h&_nc_ohc=XFrdg0Tkl2YAX8PSIWI&tn=OPVFv6OQ3h4Ncu_e&_nc_ht=scontent.feoh6-1.fna&oh=00_AT-jQeW7VhMz_hcntGhcqJPXWm8n5MuzGlLMZrNnJuAQdA&oe=629F1E65",
+        img: "https://scontent.feoh2-1.fna.fbcdn.net/v/t39.30808-6/274554932_5063805380343382_5018588772167601835_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeEtF-ymBimvMLdHgF5FrVq1sKgb8UIOExqwqBvxQg4TGjxFBJTxbwp_kb3pbcZ_C8dbeGXecKuyzgu-0O7w1q3h&_nc_ohc=iGPSSzuFgJUAX_fPM3A&tn=OPVFv6OQ3h4Ncu_e&_nc_ht=scontent.feoh2-1.fna&oh=00_AT_NhWvCJOKLFT_2DyxQGi9W8jqPh0Gu5fRq3zx5gtqS1w&oe=62A50D25",
     },
     {
         id: 5,
         price: 6.000,
         amount: 0,
         name: "Porción de Papas",
-        img: "https://scontent.feoh6-1.fna.fbcdn.net/v/t39.30808-6/274715802_5070076119716308_4170852015525030876_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeG28KmZuD9d-4twpdde4DSrtv9-3FDDp4u2_37cUMOni74ipaZbAPbtH8TdsobxRAhKt0Sib7yvHXek16TWAH-9&_nc_ohc=NtRUuJktVvYAX_NjfNx&_nc_ht=scontent.feoh6-1.fna&oh=00_AT8GD2clewQaXDgR_h1J0hA72WjuX6eJv1ev7x7vE8CKyg&oe=629F685C",
+        img: "https://scontent.feoh2-1.fna.fbcdn.net/v/t39.30808-6/274715802_5070076119716308_4170852015525030876_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeG28KmZuD9d-4twpdde4DSrtv9-3FDDp4u2_37cUMOni74ipaZbAPbtH8TdsobxRAhKt0Sib7yvHXek16TWAH-9&_nc_ohc=946LCl0iLMIAX_UtCVp&_nc_ht=scontent.feoh2-1.fna&oh=00_AT_G94qwUdO6zFXeMT_gJ0MKq5dg3bWvnBqlCJtqCbdZoQ&oe=62A5571C",
     },
     {
         id: 6,
